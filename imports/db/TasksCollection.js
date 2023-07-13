5b4978845b3697a7be8b8a8bd867398bc8a302ba
Ayush Ganguly <ayushganguly@Ayushs-MacBook-Pro-2.local>
2023-07-12 21:26:55 -0400

    Revert "Task Manager App"
    This reverts commit ee1d1a40faba7244d4509ea391755f11c8340453.

--- imports/db/TasksCollection.js
+++
@@ -1,8 +0,0 @@
-import { Mongo } from 'meteor/mongo'
-
-export const TasksCollection = new Mongo.Collection('tasks');
-
-//tasks is name of our collection in the MongoDb database
-
-//TasksCollection is the name we use to represent the collection 'tasks'
-//in react 
